78d6558934cf93c81ef31b635eb98c2f15138f51
krisztian2545 <40322823+krisztian2545@users.noreply.github.com>
2024-05-26 10:58:55 +0200

    export the core package in the other packages

--- packages/item_store_flutter/lib/item_store_flutter.dart
+++ packages/item_store_flutter/lib/item_store_flutter.dart
@@ -2,3 +2,4 @@ library item_store_flutter;
 
 export 'src/item_store_provider.dart';
 export 'src/context_extensions.dart';
+export 'package:item_store/item_store.dart';
--- packages/signal_store/lib/signal_store.dart
+++ packages/signal_store/lib/signal_store.dart
@@ -2,3 +2,4 @@ library signal_store;
 
 export 'src/signals_utils.dart';
 export 'package:signals_core/signals_core.dart';
+export 'package:item_store/item_store.dart';
