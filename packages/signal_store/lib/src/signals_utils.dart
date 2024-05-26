125e0ba71ebdd966decf06980bd8e9667e004d61
krisztian2545 <40322823+krisztian2545@users.noreply.github.com>
2024-05-26 11:43:50 +0200

    added a builder for signalFactory

--- packages/signal_store/lib/src/signals_utils.dart
+++ packages/signal_store/lib/src/signals_utils.dart
@@ -1,16 +1,22 @@
 import 'package:item_store/item_store.dart';
 import 'package:signals_core/signals_core.dart';
 
-ItemFactory<Signal<T>> signalFactory<T>(
-  T value, {
+ItemFactory<Signal<T>> signalFactory<T>({
+  T? value,
+  T Function(Ref)? builder,
   String? debugLabel,
   bool autoDispose = false,
-}) =>
-    (Ref ref) => ref.registerDisposable(signal(
-          value,
-          debugLabel: debugLabel,
-          autoDispose: autoDispose,
-        )..onDispose(() => ref.disposeSelf()));
+}) {
+  assert(
+    (value != null && builder == null) || (value == null && builder != null),
+    "Either value or builder must be specified, but not both.",
+  );
+  return (Ref ref) => ref.registerDisposable(signal(
+        builder?.call(ref) ?? value!,
+        debugLabel: debugLabel,
+        autoDispose: autoDispose,
+      )..onDispose(() => ref.disposeSelf()));
+}
 
 ItemFactory<Computed<T>> computedFactory<T>(
   T Function() compute, {
