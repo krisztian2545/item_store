70122618d416d560b3081394214933a390c35255
krisztian2545 <40322823+krisztian2545@users.noreply.github.com>
2024-08-11 17:01:32 +0200

    added generics to signal getter on ItemStore, Ref and WidgetRef

--- packages/signal_store/lib/src/store_signal_utils.dart
+++ packages/signal_store/lib/src/store_signal_utils.dart
@@ -2,10 +2,10 @@ import 'package:signal_store/signal_store.dart';
 
 extension StoreSignalUtilsX on ItemStore {
   Signal<T> signal<T>(Object globalKey) =>
-      get((_) => lazySignal<T>(), globalKey: globalKey);
+      get<Signal<T>>((_) => lazySignal<T>(), globalKey: globalKey);
 }
 
 extension RefSignalUtilsX on Ref {
   Signal<T> signal<T>(Object globalKey) =>
-      this((_) => lazySignal<T>(), globalKey: globalKey);
+      this<Signal<T>>((_) => lazySignal<T>(), globalKey: globalKey);
 }
--- packages/signal_store_flutter/lib/src/signal_utils.dart
+++ packages/signal_store_flutter/lib/src/signal_utils.dart
@@ -2,5 +2,5 @@ import 'package:signal_store_flutter/signal_store_flutter.dart';
 
 extension WidgetRefSignalUtilsExtensionX on WidgetRef {
   Signal<T> signal<T>(Object globalKey) =>
-      this((_) => lazySignal<T>(), globalKey: globalKey);
+      this<Signal<T>>((_) => lazySignal<T>(), globalKey: globalKey);
 }
