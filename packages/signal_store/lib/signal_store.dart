fcf198055b6720561898b319c7386ce4d83ba978
krisztian2545 <40322823+krisztian2545@users.noreply.github.com>
2024-08-11 16:53:15 +0200

    fix: export store_signal_utils.dart

--- packages/signal_store/lib/signal_store.dart
+++ packages/signal_store/lib/signal_store.dart
@@ -4,6 +4,7 @@ export 'src/computed_factories.dart';
 export 'src/effect_factories.dart';
 export 'src/ref_extensions.dart';
 export 'src/signal_factories.dart';
+export 'src/store_signal_utils.dart';
 export 'src/stream_factories.dart';
 
 // re-export packages
