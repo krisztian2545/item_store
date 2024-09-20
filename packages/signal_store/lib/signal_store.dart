4af84a9207cfdea546514ad10710bb301ab822bc
krisztian2545 <40322823+krisztian2545@users.noreply.github.com>
2024-09-20 15:57:55 +0200

    fix: added missing export

--- packages/signal_store/lib/signal_store.dart
+++ packages/signal_store/lib/signal_store.dart
@@ -2,6 +2,7 @@ library signal_store;
 
 export 'src/computed_factories.dart';
 export 'src/effect_factories.dart';
+export 'src/factory_params.dart';
 export 'src/ref_extensions.dart';
 export 'src/signal_factories.dart';
 export 'src/store_signal_utils.dart';
