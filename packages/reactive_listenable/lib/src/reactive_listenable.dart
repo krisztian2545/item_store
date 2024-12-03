1adea25b0ae15f6ef1aa2f9f96d3344020402428
krisztian2545 <40322823+krisztian2545@users.noreply.github.com>
2024-12-03 22:34:48 +0100

    chore!: removed reactive_listenables, since it's moved to a separate repo

--- packages/reactive_listenable/lib/src/reactive_listenable.dart
+++
@@ -1,9 +0,0 @@
-export 'async_state.dart';
-export 'batch.dart';
-export 'change_observer.dart';
-export 'controller_utils.dart';
-export 'notifier_extensions.dart';
-export 'reactive.dart';
-export 'readonly_state_notifier.dart';
-export 'state_notifier.dart';
-export 'widgets/widgets.dart';
--- packages/reactive_listenable/lib/src/readonly_state_notifier.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'disposable_mixin.dart';
-
-import 'state_notifier.dart';
-
-extension type ReadonlyStateNotifier<T>(StateNotifier<T> _notifier)
-    implements ValueListenable<T>, ObservedDisposableMixin {}
