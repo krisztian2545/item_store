6a88045b6067e5c853a3bdcf5de72468211a07eb
krisztian2545 <40322823+krisztian2545@users.noreply.github.com>
2025-06-03 21:09:52 +0200

    fix: updated api

--- packages/signal_store_flutter/lib/src/signal_utils.dart
+++ packages/signal_store_flutter/lib/src/signal_utils.dart
@@ -3,7 +3,7 @@ import 'package:signals_flutter/signals_flutter.dart';
 
 extension WidgetRefSignalUtilsExtensionX on WidgetRef {
   Signal<T> signal<T>(Object key) =>
-      this<Signal<T>>((_) => lazySignal<T>(), globalKey: key);
+      this<Signal<T>>((_) => lazySignal<T>(), key: key);
 
   ReadonlySignal<T> subToSignal<T>(ReadonlySignal<T> signal) {
     callOnce(signal.sub, tag: (subToSignal, signal));
