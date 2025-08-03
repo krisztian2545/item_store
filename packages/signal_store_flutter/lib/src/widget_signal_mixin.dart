01a9f50e462436a11845e855fc1aaf16b66ca789
krisztian2545 <40322823+krisztian2545@users.noreply.github.com>
2025-08-03 19:38:11 +0200

    feat: added oldWidget to widgetSignal

--- packages/signal_store_flutter/lib/src/widget_signal_mixin.dart
+++ packages/signal_store_flutter/lib/src/widget_signal_mixin.dart
@@ -2,12 +2,13 @@ import 'package:flutter/widgets.dart';
 import 'package:signals_flutter/signals_flutter.dart';
 
 mixin WidgetSignalMixin<SF extends StatefulWidget> on State<SF> {
-  late final Signal<SF> widgetSignal = Signal(widget);
+  late final Signal<({SF? oldWidget, SF widget})> widgetSignal =
+      Signal((oldWidget: null, widget: widget));
 
   @override
   void didUpdateWidget(covariant oldWidget) {
     super.didUpdateWidget(oldWidget);
-    widgetSignal.value = widget;
+    widgetSignal.value = (oldWidget: oldWidget, widget: widget);
   }
 
   @override
