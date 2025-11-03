cdc0de729d2f6d9958f8443615777074e7ccdd2d
krisztian2545 <40322823+krisztian2545@users.noreply.github.com>
2025-11-03 03:40:50 +0100

    fix: signal dispose loop on binding

--- packages/signal_store/lib/src/ref_extensions.dart
+++ packages/signal_store/lib/src/ref_extensions.dart
@@ -18,27 +18,35 @@ extension SignalsRefUtilsX on Ref {
     return cleanup;
   }
 
+  R bindToSignal<R extends ReadonlySignal<T>, T>(R signal) {
+    signal.bindTo(this, dispose: (signal) {
+      if (signal.disposed) return;
+      signal.dispose();
+    });
+    return signal;
+  }
+
   Signal<T> boundSignal<T>(
     T value, {
     String? debugLabel,
     bool autoDispose = true,
   }) =>
-      Signal<T>(
+      bindToSignal(Signal<T>(
         value,
         debugLabel: debugLabel,
         autoDispose: autoDispose,
-      )..bindTo(this);
+      ));
 
   Computed<T> boundComputed<T>(
     T Function() fn, {
     String? debugLabel,
     bool autoDispose = true,
   }) =>
-      Computed<T>(
+      bindToSignal(Computed<T>(
         fn,
         debugLabel: debugLabel,
         autoDispose: autoDispose,
-      )..bindTo(this);
+      ));
 
   FutureSignal<T> boundComputedAsync<T>(
     Future<T> Function() fn, {
@@ -48,14 +56,14 @@ extension SignalsRefUtilsX on Ref {
     List<ReadonlySignal<dynamic>> dependencies = const [],
     bool lazy = true,
   }) =>
-      FutureSignal<T>(
+      bindToSignal(FutureSignal<T>(
         fn,
         initialValue: initialValue,
         debugLabel: debugLabel,
         autoDispose: autoDispose,
         dependencies: dependencies,
         lazy: lazy,
-      )..bindTo(this);
+      ));
 
   void cancelSignalDependency(ReadonlySignal signal) {
     local.disposeItem((signalDependency: signal));
